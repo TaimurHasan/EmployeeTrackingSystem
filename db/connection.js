79131fdf25d2551ca379f34d4583b84538b58d66
TaimurHasan <taimurhasan11@gmail.com>
2022-05-06 16:14:46 -0400

    setting up initial structure and initializing mysql

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+// set up sql connection
+const db = mysql.createConnection({
+    host:'localhost',
+    user: process.env.user,
+    password: process.env.password,
+    database: 'organization'
+},
+console.log('Connected to the organization database')
+);
+
+module.exports = db;
