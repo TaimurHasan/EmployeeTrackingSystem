5ebeee8be7974b403e2d93e93b47f9909f3b3219
TaimurHasan <taimurhasan11@gmail.com>
2022-05-06 18:13:06 -0400

    added get all roles function and response

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ const db = require('./db/connection');
 const orgQuery = require('./lib/queries');
 
 // main inquirer prompt function to provide actions to user
-inquirer.prompt([
+const promptMainMenu = () => {
+    inquirer.prompt([
     {
         name: 'action',
         type: 'list',
         message: 'What would you like to do?',
         choices: ['View all departments', 'View all roles', 'View all employees', 'add a department', 'add a role', 'add an employee', 'update employee role']
     }
-])
-.then(({ action }) => {
-    const step = new orgQuery;
-    step.getAllDepartments();
-});
+    ])
+    .then(({ action }) => {
+        const queryObj = new orgQuery;
+        switch(action) {
+            case 'View all departments':
+                queryObj.getAllDepartments();
+                break;
+            case 'View all roles':
+                queryObj.getAllRoles();
+                break;
+            default: 
+                console.log('Pending');
+        }
+    });
+};
+
+promptMainMenu();
